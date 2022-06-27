4ab0fdbe6ea9ac811dbcd28825755e39f8a678a7
dahoon06 <dahoon226@naver.com>
2022-06-27 13:04:29 +0900

    style: Presentational and Container Component Pattern

---
+++ frontend/src/components/Common/Home/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+// markup 없이 데이터만 presnter에게 props 로 내림
+const HomeContainer = () => {
+  return (
+    <HomePresenter />
+  )
+}
+
+export default HomeContainer;
