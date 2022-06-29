165c88c6cf7c09983e530a46fbe990a8dbd87618
dahoon06 <dahoon226@naver.com>
2022-06-29 18:10:40 +0900

    feat: 라우터 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './assets/images/icons/logo.svg';
 import './assets/style/App.scss';
+import { RouteController } from "./routes";
+import Footer from "./components/Common/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouteController />
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { MemberRouter } from "./Member";
+import { DottoBoardRouter } from "./DottoBoard";
+import { MainPage } from "../pages/MainPage";
+
+export const RouteController = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to='/dotto' />} />
+      <Route path='/dotto' element={<MainPage />} />
+      <Route path='/dotto-board/*' element={<DottoBoardRouter />} />
+      <Route path='/user/*' element={<MemberRouter />} />
+    </Routes>
+  );
+};
